c5e74c00924908b7ad29760223fd771ae8a887d5
mohammad <samplemailer444@gmail.com>
2017-10-22 22:55:49 -0400

    finished template

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ const express = require('express'),
 
 
 app.set('view engine', 'ejs');
+
+
+
+
 app.use(express.static('public'));
 
-app.get('/',(req,res)=> res.render('index'));
+app.get('/profile',(req,res)=> {
+    let user = {
+        name: 'Joe',
+        img: 'http://www.how-to-draw-funny-cartoons.com/images/cartoon-boy-004.jpg'
+    }
+    res.render('profile',{user: user})
+
+}); //localhost:3000/
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
 
 app.listen(port,(err)=>{
         if(!err){
